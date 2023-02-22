51f8a81e58f4de578bbe61f6fd8e07e41d211a80
Rafael Ramos <ingrnrg@gmail.com>
2023-02-22 13:24:36 -0600

    added errorMiddleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const colors = require('colors') // eslint-disable-line no-unused-vars
 
 /* External Functions */
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/errorMiddleware')
 
 // Connection with the port
 const port = process.env.PORT
@@ -16,6 +17,9 @@ connectDB()
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// Use middleware
+app.use(errorHandler)
 
 // Conecction APP whit the port
 app.listen(port, () => console.log(`Server started on port ${port}`))
